3a69fd6732fcffcd063700052037f7a182562ff8
awundersee <andreas.wundersee@iu-study.org>
2025-11-12 20:37:13 +0100

    Forum API

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 const API_BASE =
   import.meta.env.MODE === "development"
     ? "http://localhost:3001/api"
-    : "http://localhost:3001/api";
+    : "https://earthcircle.wundersee.dev/api";
 
 export default API_BASE;
